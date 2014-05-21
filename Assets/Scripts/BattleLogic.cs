e151efe4eb076ac33d1eae515473ddc65c5dd8d1
saforer <saforer@gmail.com>
2014-05-21 03:39:39 -0400

    Init Commit is the shit
    But realtalk, it's just basic UI testing

---
+++ Assets/Scripts/BattleLogic.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleLogic : MonoBehaviour {
+
+    private BattleUI currentUI;
+    private int selectedInt = 0;
+    private MenuOptions selectedOption = MenuOptions.Attack;
+
+    void Start ()
+    {
+        currentUI = gameObject.GetComponent<BattleUI>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            selectedInt++;
+            if(selectedInt>2)
+            {
+                selectedInt = 2;
+            }
+            EvalSelectedOption();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            selectedInt--;
+            if(selectedInt<0)
+            {
+                selectedInt = 0;
+            }
+            EvalSelectedOption();
+        }
+    }
+
+    void EvalSelectedOption()
+    {
+        switch (selectedInt)
+        {
+            case 0:
+                selectedOption = MenuOptions.Attack;
+                break;
+            case 1:
+                selectedOption = MenuOptions.MetaMagic;
+                break;
+            case 2:
+                selectedOption = MenuOptions.Run;
+                break;
+        }
+
+        UpdateUI(selectedOption);
+    }
+
+    void UpdateUI(MenuOptions update)
+    {
+        currentUI.attackMenuState = MenuButtonState.Off;
+        currentUI.metaMagicMenuState = MenuButtonState.Off;
+        currentUI.runMenuState = MenuButtonState.Off;
+
+        switch (update)
+        {
+            case MenuOptions.Attack:
+                currentUI.attackMenuState = MenuButtonState.On;
+                break;
+            case MenuOptions.MetaMagic:
+                currentUI.metaMagicMenuState = MenuButtonState.On;
+                break;
+            case MenuOptions.Run:
+                currentUI.runMenuState = MenuButtonState.On;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/BattleUI.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MenuOptions 
+{
+	Attack,
+	MetaMagic,
+	Run
+}
+
+public enum MenuButtonState
+{
+    On,
+    Off,
+    Locked
+}
+
+public class BattleUI : MonoBehaviour 
+{
+
+    public Texture menuBoxBackground;
+
+
+    public Texture menuOptionTextureOff;
+    public Texture menuOptionTextureOn;
+    public Texture menuOptionTextureLock;
+
+    private Texture attackMenuTexture;
+    private Texture metaMagicMenuTexture;
+    private Texture runMenuTexture;
+
+    [HideInInspector]
+    public MenuButtonState attackMenuState;
+    [HideInInspector]
+    public MenuButtonState metaMagicMenuState;
+    [HideInInspector]
+    public MenuButtonState runMenuState;
+    
+	// Use this for initialization
+	void Start () 
+    {
+        attackMenuTexture = menuOptionTextureOff;
+        metaMagicMenuTexture = menuOptionTextureOff;
+        runMenuTexture = menuOptionTextureOff;
+
+        attackMenuState = MenuButtonState.On;
+        metaMagicMenuState = MenuButtonState.Off;
+        runMenuState = MenuButtonState.Off;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+	void OnGUI () 
+    {
+        DrawMenuBox();
+        DrawAttack();
+        DrawMetaMagic();
+        DrawRun();
+
+	}
+
+    void DrawMenuBox()
+    {
+        GUI.DrawTexture(new Rect(10, 10, 100, 100), menuBoxBackground);
+    }
+
+    void DrawAttack()
+    {
+
+        switch (attackMenuState)
+        {
+        case MenuButtonState.On:
+            attackMenuTexture = menuOptionTextureOn;
+            break;
+        case MenuButtonState.Off:
+            attackMenuTexture = menuOptionTextureOff;
+            break;
+        case MenuButtonState.Locked:
+            attackMenuTexture = menuOptionTextureLock;
+            break;
+        }        
+
+        GUI.DrawTexture(new Rect(10, 10, 100, 20), attackMenuTexture);
+        GUI.Label(new Rect(45, 10, 100, 20), "Attack");
+    }
+
+    void DrawMetaMagic()
+    {
+        switch (metaMagicMenuState)
+        {
+        case MenuButtonState.On:
+            metaMagicMenuTexture = menuOptionTextureOn;
+            break;
+        case MenuButtonState.Off:
+            metaMagicMenuTexture = menuOptionTextureOff;
+            break;
+        case MenuButtonState.Locked:
+            metaMagicMenuTexture = menuOptionTextureLock;
+            break;
+        }        
+        
+        GUI.DrawTexture(new Rect(10, 30-3, 100, 20), metaMagicMenuTexture);
+        GUI.Label(new Rect(30, 30-3, 100, 20), "MetaMagic");
+    }
+
+    void DrawRun()
+    {
+        switch (runMenuState)
+        {
+        case MenuButtonState.On:
+            runMenuTexture = menuOptionTextureOn;
+            break;
+        case MenuButtonState.Off:
+            runMenuTexture = menuOptionTextureOff;
+            break;
+        case MenuButtonState.Locked:
+            runMenuTexture = menuOptionTextureLock;
+            break;
+        }        
+        
+        GUI.DrawTexture(new Rect(10, 50-6, 100, 20), runMenuTexture);
+        GUI.Label(new Rect(50, 50-6, 100, 20), "Run");
+    }
+
+}
