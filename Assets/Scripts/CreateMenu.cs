e57d377564157b492f3fd2cdb2b99ce196065369
saforer <saforer@gmail.com>
2014-05-23 08:41:23 -0400

    Well let's try doing this again, but this time possibly from a different area?

---
+++ Assets/Scripts/CreateMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMenu {
+
+	RPGmenu InitiateMainMenu(RPGmenu inMenu)
+    {
+        RPGmenu outMenu = new RPGmenu(0);
+
+        
+
+
+
+        return outMenu;
+    }
+}
